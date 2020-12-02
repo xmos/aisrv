b3979a80aa0bf227f4d0ba8d092de336ac5511c3
Henk Muller <henk@xmos.com>
2020-12-02 17:27:39 +0000

    Added python file with command numbers

---
+++ app_aisrv_spi/aisrv_cmd.py
@@ -0,0 +1,13 @@
+CMD_GET_STATUS        = int(0x01)
+CMD_GET_ID            = int(0x03)
+CMD_GET_SPEC          = int(0x05)
+CMD_GET_TENSOR        = int(0x07)
+CMD_GET_TIMINGS       = int(0x09)
+
+CMD_SET_MODEL         = int(0x02)
+CMD_SET_SERVER        = int(0x04)
+CMD_SET_TENSOR        = int(0x06)
+
+CMD_START_INFER       = int(0x08)
+CMD_START_ACQUIRE     = int(0x0A)
+CMD_HELLO             = int(0x55)
