0467c68d84ed46e4e09acd272bb1162369f0ad66
xross <ross@xmos.com>
2020-12-02 13:41:19 +0000

    send_picture.py mods for SPI

--- app_aisrv_spi/send_picture.py
+++ app_aisrv_spi/send_picture.py
@@ -6,32 +6,18 @@ import os
 import time
 import struct
 import ctypes
-import cv2
+from math import sqrt
 
 import numpy as np
-from matplotlib import pyplot
 
 import usb.core
 import usb.util
 
+from xcore_ai_ie import xcore_ai_ie_spi
+
+
 DRAW = False
-SEND_MODEL = False
-MODEL_PATH = "./model/model_quant_xcore.tflite"
 
-# Commands - TODO properly share with app code
-CMD_LENGTH_BYTES = 1
-
-CMD_NONE = 0
-CMD_GET_OUTPUT_LENGTH = 1
-CMD_SET_INPUT_TENSOR = 2
-CMD_START_INFER = 3
-CMD_GET_OUTPUT_TENSOR = 4
-CMD_SET_MODEL = 5
-###
-
-
-MAX_PACKET_SIZE = 512 # TODO read from device
-INPUT_SHAPE = (128, 128, 3)
 INPUT_SCALE = 0.007843137718737125
 INPUT_ZERO_POINT = -1
 NORM_SCALE = 127.5
@@ -57,7 +43,8 @@ PRINT_CALLBACK = ctypes.CFUNCTYPE(
     None, ctypes.c_ulonglong, ctypes.c_uint, ctypes.c_char_p
 )
 
-
+# TODO use quantize/dequantize from ai_tools
+#from tflite2xcore.utils import quantize, dequantize   
 def quantize(arr, scale, zero_point, dtype=np.int8):
     t = np.round(arr / scale + zero_point)
     return dtype(np.round(np.clip(t, np.iinfo(dtype).min, np.iinfo(dtype).max)))
@@ -66,143 +53,69 @@ def quantize(arr, scale, zero_point, dtype=np.int8):
 def dequantize(arr, scale, zero_point):
     return np.float32((arr.astype(np.int32) - np.int32(zero_point)) * scale)
 
-# find our device
-dev = None
-while dev is None:
-    dev = usb.core.find(idVendor=0x20b1) #, idProduct=0xa15e)
 
-# was it found?
-if dev is None:
-    raise ValueError('Device not found')
+ie = xcore_ai_ie_spi()
 
-# set the active configuration. With no arguments, the first
-# configuration will be the active one
-dev.set_configuration()
+ie.connect()
 
-# get an endpoint instance
-cfg = dev.get_active_configuration()
+input_length = ie.input_length
+print("READING INPUT TENSOR LENGTH FROM DEVICE: " + str(input_length))
 
-#print("found device: \n" + str(cfg))
-intf = cfg[(0,0)]
+output_length = ie.output_length
+print("READING OUTPUT TENSOR LENGTH FROM DEVICE: " + str(output_length))
 
-out_ep = usb.util.find_descriptor(
-    intf,
-    # match the first OUT endpoint
-    custom_match = \
-    lambda e: \
-        usb.util.endpoint_direction(e.bEndpointAddress) == \
-        usb.util.ENDPOINT_OUT)
+input_shape_channels = 3
+input_shape_spacial =  int(sqrt(input_length/input_shape_channels))
+INPUT_SHAPE = (input_shape_spacial, input_shape_spacial, input_shape_channels)
 
-in_ep = usb.util.find_descriptor(
-    intf,
-    # match the first OUT endpoint
-    custom_match = \
-    lambda e: \
-        usb.util.endpoint_direction(e.bEndpointAddress) == \
-        usb.util.ENDPOINT_IN)
-
-assert out_ep is not None
-assert in_ep is not None
-
-print("Connected")
-
-if SEND_MODEL:
-
-    print("WRITING MODEL VIA USB..\n")
-
-    with open(MODEL_PATH, "rb") as input_fd:
-        input_model = input_fd.read()
-
-    model_bytes = bytearray(input_model)
-
-    print("Model length (bytes): " + str(len(model_bytes)))
-
-    #Send model to device 
-    out_ep.write(bytes([CMD_SET_MODEL]))
-
-    # Send model size
-    len_bytes = int.to_bytes(len(model_bytes), byteorder = "little", signed=True, length=4)
-    out_ep.write(len_bytes, 1000)
-    
-    out_ep.write(model_bytes, 1000)
-
-    print("FINISHED WRITING MODEL")
-
-
-# Get output size from device
-out_ep.write(bytes([CMD_GET_OUTPUT_LENGTH]), 50000)
-
-try:
-    output_length = int.from_bytes(dev.read(in_ep, 4, 10000), byteorder = "little", signed=True)
-    print("READING OUTPUT TENSOR LENGTH FROM DEVICE: " + str(output_length))
-except usb.core.USBError as e:
-
-    if e.backend_error_code == usb.backend.libusb1.LIBUSB_ERROR_PIPE:
-        print("Device error, IN pipe halted (no model uploaded?)")
-        
-    sys.exit(1)
-
+print("Inferred input shape: " + str(INPUT_SHAPE))
 
 raw_img = None
 
 # Send image to device
-
 for arg in sys.argv[1:]:
         print("SETTING INPUT TENSOR VIA USB\n")
         try:
-            img = cv2.imread(arg)
-            img = cv2.resize(img, (INPUT_SHAPE[0], INPUT_SHAPE[1]))
+            if not arg.endswith('.raw'):
+                import cv2
+                img = cv2.imread(arg)
+                img = cv2.resize(img, (INPUT_SHAPE[0], INPUT_SHAPE[1]))
             
-            # Channel swapping due to mismatch between open CV and XMOS
-            img = img[:, :, ::-1]  # or image = image[:, :, (2, 1, 0)]
+                # Channel swapping due to mismatch between open CV and XMOS
+                img = img[:, :, ::-1]  # or image = image[:, :, (2, 1, 0)]
 
-            img = (img / NORM_SCALE) - NORM_SHIFT
-            img = np.round(quantize(img, INPUT_SCALE, INPUT_ZERO_POINT))
+                img = (img / NORM_SCALE) - NORM_SHIFT
+                img = np.round(quantize(img, INPUT_SCALE, INPUT_ZERO_POINT))
 
-            raw_img = bytes(img)
-
-            out_ep.write(bytes([CMD_SET_INPUT_TENSOR]))
-
-            sentcount = 0
-            for i in range(0, len(raw_img), MAX_PACKET_SIZE):
-                out_ep.write(raw_img[i : i + MAX_PACKET_SIZE])
-                sentcount = sentcount + MAX_PACKET_SIZE
-                size_str = "sent: " + str(sentcount)
-                sys.stdout.write('%s\r' % size_str)
-                sys.stdout.flush()
-           
+                raw_img = bytes(img)
             
-            sys.stdout.write('%s.. Done\n'  % size_str)
+            else:
+                raw_file = open(arg, 'rb')
+                raw_img = raw_file.read()
+                raw_file.close()
+            
+            #newFile = open("raw_img.raw", "wb")
+            #newFile.write(raw_img)
+            ie.write_input_tensor(raw_img)
                 
         except KeyboardInterrupt:
             pass
 
         print("Sending start inference command")
-        out_ep.write(bytes([CMD_START_INFER]), 1000)
+        ie.start_inference()
 
         print("Waiting for inference")
-        out_ep.write(bytes([CMD_GET_OUTPUT_TENSOR]), 50000)
-
-        # Retrieve result from device
-        # TODO deal with len(output_data > MAX_PACKET_SIZE)
-        #output_data = dev.read(in_ep, output_length, 1000)
-        output_data = dev.read(in_ep, output_length, 10000)
-
-        output_data_int = []
-
-        # TODO better way of doing this?
-        for i in output_data:
-            x =  int.from_bytes([i], byteorder = "little", signed=True)
-            output_data_int.append(x)
+        output_data_int = ie.read_output_tensor()
 
         max_value = max(output_data_int)
         max_value_index = output_data_int.index(max_value)
 
         prob = (max_value - OUTPUT_ZERO_POINT) * OUTPUT_SCALE * 100.0
-        print("Output tensor read as ", str(output_data_int), ", this is a " + OBJECT_CLASSES[max_value_index], f"{prob:0.2f}%")
+        print("Output tensor read as ", str(output_data_int),", this is a " + OBJECT_CLASSES[max_value_index], f"{prob:0.2f}%")
 
         if DRAW: 
 
+            from matplotlib import pyplot
             np_img = np.frombuffer(raw_img, dtype=np.int8).reshape(INPUT_SHAPE)
             np_img = np.round(
                 (dequantize(np_img, INPUT_SCALE, INPUT_ZERO_POINT) + NORM_SHIFT) * NORM_SCALE
