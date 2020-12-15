24635b4a2efe374ce435fb2eb56f34e77cafab62
xross <ross@xmos.com>
2020-12-15 12:07:59 +0000

    Added test

---
+++ app_aisrv_spi/tests/usb/test_model_download_upload.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Copyright (c) 2020, XMOS Ltd, All rights reserved
+
+# TODO fix sys hack
+import sys
+sys.path.append('../../../host_python/')
+
+from xcore_ai_ie import xcore_ai_ie_usb
+
+ie = xcore_ai_ie_usb()
+
+ie.connect()
+
+model_file1 = "../../../model/model_quant_xcore.tflite"
+
+# Read model file
+with open(model_file1, "rb") as input_fd:
+    model_data1 = input_fd.read()
+
+CYCLE_COUNT = 5
+
+for x in range(CYCLE_COUNT):
+
+    # Send to device
+    ie.download_model(bytearray(model_data1))
+
+    # Read back model and check
+    model_read = ie.upload_model()
+    assert model_read == bytearray(model_data1)
+
+
+
+
+
