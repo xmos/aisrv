9b04299263c1f41aa26630569cfa169f73a4c1fc
Henk Muller <henk@xmos.com>
2021-08-12 13:33:01 +0100

    Refactored code so that it now uses the tensor-arena or an external memory for the model, the latter can be a null-ptr

--- app_alpr/src/aisrv_conf.h
+++ app_alpr/src/aisrv_conf.h
@@ -1,9 +1,6 @@
 #ifndef _AISRV_CONF_H_
 #define _AISRV_CONF_H_
 
-#define INT_MEM_SIZE_BYTES          (2300000)
-#define MAX_MODEL_SIZE_EXT_BYTES    (10224000)
-
 #define NETWORK_NUM_THREADS         (1)
 
 #define AISRV_GPIO_LENGTH           (4)
--- app_alpr/src/server_memory.cc
+++ app_alpr/src/server_memory.cc
@@ -10,17 +10,21 @@
 #if !defined(TFLM_DISABLED)
 static struct tflite_micro_objects s0;
 
-__attribute__((section(".ExtMem_data")))
-uint8_t data_ext[MAX_MODEL_SIZE_EXT_BYTES] __attribute__((aligned(4)));
 
+#define TENSOR_ARENA_BYTES          (10224000)
+
+// Because of bug in xgdb we make this array tiny, knowing we own external memory
+// otherwise xgdb spends hours loading this array
+// TODO - fix when bug is fixed.
 __attribute__((section(".ExtMem_data")))
-uint8_t data_int[INT_MEM_SIZE_BYTES] __attribute__((aligned(4)));
+uint8_t data_ext[16 + 0 * TENSOR_ARENA_BYTES] __attribute__((aligned(4)));
 #endif
 
 void inference_engine_initialize_with_memory(inference_engine_t *ie) {
 #if !defined(TFLM_DISABLED)
-    inference_engine_initialize(ie, data_int, INT_MEM_SIZE_BYTES,
-                                data_ext, MAX_MODEL_SIZE_EXT_BYTES,
+    inference_engine_initialize(ie,
+                                data_ext, TENSOR_ARENA_BYTES,
+                                nullptr,  0,
                                 &s0);
     
 #endif
