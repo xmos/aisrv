2ecba24c7730753ab3d35a80927424d6dcb53541
Henk Muller <henk@xmos.com>
2021-08-25 08:28:56 +0100

    Moved sensor and image size to the appropriate location

--- app_alpr/src/aisrv_mipi.h
+++ app_alpr/src/aisrv_mipi.h
@@ -1,6 +1,13 @@
-
 #ifndef _AISRV_MIPI_H_
 #define _AISRV_MIPI_H_
+
+#define RAW_IMAGE_HEIGHT (300)
+#define RAW_IMAGE_WIDTH  (300)
+#define RAW_IMAGE_DEPTH  (3)
+
+#define SENSOR_IMAGE_HEIGHT (480)
+#define SENSOR_IMAGE_WIDTH  (640)
+#define SENSOR_IMAGE_DEPTH  (2)          // YUV: YU  YV  YU  YV  ...
 
 #include "i2c.h"
 
