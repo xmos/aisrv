1af5cb6f6d24d33b4fa7821ec59c2cf36bd6669b
Henk Muller <henk@xmos.com>
2021-09-02 11:46:07 +0100

    SUpport to send models to multiple engines

--- host_python/send_model.py
+++ host_python/send_model.py
@@ -4,6 +4,9 @@
 import sys
 from xcore_ai_ie import xcore_ai_ie_usb, xcore_ai_ie_spi, AISRVError
 
+if len(sys.argv) < 3:
+    print("Usage: python3 " + sys.argv[0] + " (usb|spi) (ext|arena) model.tflit")
+    exit(1)
 if sys.argv[1] == 'usb':
     ie = xcore_ai_ie_usb()
 elif sys.argv[1] == 'spi':
@@ -22,11 +25,13 @@ else:
 
 ie.connect()
 
-try:
-    ie.download_model_file(sys.argv[3], ext_mem = ext_mem)
-except AISRVError:
-    print("Device reported an error")
-debug_string = ie.read_debug_log()
-print("Debug log from device: " +  str(debug_string))
-
+engine_num = 0
+for model in sys.argv[3:]:
+    try:
+        ie.download_model_file(model, ext_mem = ext_mem, engine_num = engine_num)
+    except AISRVError:
+        print("Device reported an error : ")
+        debug_string = ie.read_debug_log()
+        print(str(debug_string))
+    engine_num += 1
 
