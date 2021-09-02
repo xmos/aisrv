e6d643db43e5fd80dfedd909a6bf69c2a2d58404
Henk Muller <henk@xmos.com>
2021-09-02 15:26:56 +0100

    Made compatible with multiple engines

--- app_alpr/src/server_memory.cc
+++ app_alpr/src/server_memory.cc
@@ -8,7 +8,6 @@
 #include "server_memory.h"
 
 #if !defined(TFLM_DISABLED)
-static struct tflite_micro_objects s0;
 
 
 #define TENSOR_ARENA_BYTES          (20224000)
@@ -22,10 +21,32 @@ uint32_t data_ext[16 + 0 * TENSOR_ARENA_BYTES/sizeof(int)];
 
 void inference_engine_initialize_with_memory(inference_engine_t *ie) {
 #if !defined(TFLM_DISABLED)
-    inference_engine_initialize(ie,
-                                data_ext, TENSOR_ARENA_BYTES,
-                                nullptr,  0,
-                                &s0);
-    
+    static struct tflite_micro_objects s0;
+    auto *resolver = inference_engine_initialize(ie,
+                                                 data_ext, TENSOR_ARENA_BYTES,
+                                                 nullptr,  0,
+                                                 &s0);
+    resolver->AddSoftmax();                    
+    resolver->AddPad();
+    resolver->AddMean();
+    resolver->AddReshape();
+    resolver->AddConcatenation();
+    resolver->AddFullyConnected();
+    resolver->AddAdd();
+    resolver->AddMaxPool2D();
+    resolver->AddAveragePool2D();
+    resolver->AddPad();
+    resolver->AddLogistic();
+    resolver->AddConv2D();
+    resolver->AddQuantize();
+    resolver->AddDepthwiseConv2D();
+    resolver->AddDequantize();
+    resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_V2_OpCode,
+               tflite::ops::micro::xcore::Register_Conv2D_V2());
+    resolver->AddCustom(tflite::ops::micro::xcore::FullyConnected_8_OpCode,
+            tflite::ops::micro::xcore::Register_FullyConnected_8());
+    resolver->AddCustom(tflite::ops::micro::xcore::Lookup_8_OpCode,
+            tflite::ops::micro::xcore::Register_Lookup_8());
+
 #endif
 }
