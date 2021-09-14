26aff849d8e98778d1c58b804fd95ab370a4c727
Henk Muller <henk@xmos.com>
2021-09-14 11:32:19 +0100

    Added functionality to set I2C through AI server

--- app_alpr/recv_picture.py
+++ app_alpr/recv_picture.py
@@ -12,16 +12,18 @@ from xcore_ai_ie import xcore_ai_ie_usb, xcore_ai_ie_spi
 ie = xcore_ai_ie_usb()
 ie.connect()
 
-INPUT_SHAPE = (32, 128, 3)
-#INPUT_SHAPE = (160, 160, 3)
-INPUT_SHAPE = (16, 66, 1)
+#INPUT_SHAPE = (32, 128, 3)
+INPUT_SHAPE = (160, 160, 3)
+#INPUT_SHAPE = (16, 66, 1)
 
 print("Input shape: " + str(INPUT_SHAPE))
 
 # Get image from device
-#    ie.start_acquire_single(200, 440, 120, 360, 24, 24)
+#ie.acquire_set_i2c(0x3C, 0xfe, 0x00)
+#ie.acquire_set_i2c(0x3C, 0x84, 0x01)
+ie.start_acquire_single(160, 480, 80, 400, 160, 160)
 #ythoraw_img = ie.read_input_tensor(engine_num = 1)
-raw_img = ie.read_output_tensor(engine_num = 1)
+raw_img = ie.read_input_tensor(engine_num = 0)
 print(len(raw_img))
 raw_img = raw_img[:INPUT_SHAPE[0]*INPUT_SHAPE[1]*INPUT_SHAPE[2]]
 print(len(raw_img))
