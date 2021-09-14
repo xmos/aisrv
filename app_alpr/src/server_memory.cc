131a8b8142999dae86e8952c703f0d244a04b515
Henk Muller <henk@xmos.com>
2021-09-14 11:33:26 +0100

    Optimised number of operators

--- app_alpr/src/server_memory.cc
+++ app_alpr/src/server_memory.cc
@@ -11,7 +11,7 @@
 
 
 #define TENSOR_ARENA_BYTES_0        (20224000)
-#define TENSOR_ARENA_BYTES_1          (300000)
+#define TENSOR_ARENA_BYTES_1          (356000)
 
 // Because of bug in xgdb we make this array tiny, knowing we own external memory
 // otherwise xgdb spends hours loading this array
@@ -28,27 +28,16 @@ void inference_engine_initialize_with_memory_0(inference_engine_t *ie) {
                                                  data_ext, TENSOR_ARENA_BYTES_0,
                                                  nullptr,  0,
                                                  &s0);
-    resolver->AddSoftmax();                    
     resolver->AddPad();
-    resolver->AddMean();
     resolver->AddReshape();
     resolver->AddConcatenation();
-    resolver->AddFullyConnected();
     resolver->AddAdd();
-    resolver->AddMaxPool2D();
-    resolver->AddAveragePool2D();
-    resolver->AddPad();
     resolver->AddLogistic();
     resolver->AddConv2D();
     resolver->AddQuantize();
     resolver->AddDepthwiseConv2D();
-    resolver->AddDequantize();
     resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_V2_OpCode,
                tflite::ops::micro::xcore::Register_Conv2D_V2());
-    resolver->AddCustom(tflite::ops::micro::xcore::FullyConnected_8_OpCode,
-            tflite::ops::micro::xcore::Register_FullyConnected_8());
-    resolver->AddCustom(tflite::ops::micro::xcore::Lookup_8_OpCode,
-            tflite::ops::micro::xcore::Register_Lookup_8());
 
 #endif
 }
@@ -61,27 +50,14 @@ void inference_engine_initialize_with_memory_1(inference_engine_t *ie) {
                                                  data_int, TENSOR_ARENA_BYTES_1,
                                                  nullptr,  0,
                                                  &s1);
-    resolver->AddSoftmax();                    
     resolver->AddPad();
-    resolver->AddMean();
-    resolver->AddReshape();
-    resolver->AddConcatenation();
-    resolver->AddFullyConnected();
     resolver->AddAdd();
     resolver->AddMaxPool2D();
     resolver->AddAveragePool2D();
-    resolver->AddPad();
-    resolver->AddLogistic();
     resolver->AddConv2D();
-    resolver->AddQuantize();
     resolver->AddDepthwiseConv2D();
-    resolver->AddDequantize();
     resolver->AddCustom(tflite::ops::micro::xcore::Conv2D_V2_OpCode,
                tflite::ops::micro::xcore::Register_Conv2D_V2());
-    resolver->AddCustom(tflite::ops::micro::xcore::FullyConnected_8_OpCode,
-            tflite::ops::micro::xcore::Register_FullyConnected_8());
-    resolver->AddCustom(tflite::ops::micro::xcore::Lookup_8_OpCode,
-            tflite::ops::micro::xcore::Register_Lookup_8());
 
 #endif
 }
