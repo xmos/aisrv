186c982229ea1e15b7a1f3aa4b0296ec589c2ab7
Aran <aranmcconnell@xmos.com>
2021-12-09 10:56:50 +0000

    host python can unpack model and print out the operator for each layer

--- host_python/send_model.py
+++ host_python/send_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # Copyright (c) 2020, XMOS Ltd, All rights reserved
 
-import sys
+import sys, os
 from xcore_ai_ie import xcore_ai_ie_usb, xcore_ai_ie_spi, AISRVError
 
 if len(sys.argv) < 3:
-    print("Usage: python3 " + sys.argv[0] + " (usb|spi) (single|split) model.tflit")
+    print("Usage: python3 " + sys.argv[0] + " (usb|spi) (single|split) model.tflite")
     exit(1)
 if sys.argv[1] == 'usb':
     ie = xcore_ai_ie_usb()
@@ -29,9 +29,15 @@ engine_num = 0
 for model in sys.argv[3:]:
     try:
         ie.download_model_file(model, secondary_memory = secondary, engine_num = engine_num)
+        with open('current_model.txt', 'w') as f:
+            f.write(model)
+            f.close()
     except AISRVError:
         print("Device reported an error : ")
         debug_string = ie.read_debug_log()
         print(str(debug_string))
+        if os.path.exists("current_model.txt"):
+            os.remove("current_model.txt")
+
     engine_num += 1
 
